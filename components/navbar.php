64c8e9fdf72ab12b253c2d31f20a544e8a0a204e
Heema-kama <heemakamaempibaijani@gmail.com>
2025-05-22 15:25:31 +0700

    24-001-Heema-Modul5

--- components/navbar.php
+++ components/navbar.php
@@ -4,6 +4,6 @@
     <ul>
         <li><a href="/student-interactive-profile/index.php">Profile</a></li>
         <li><a href="/student-interactive-profile/pages/timeline.php">Timeline</a></li>
-        <li><a href="/student-interactive-profile/pages/blog.php">Blog</a></li>
+        <li><a href="/240441100001_Heema Kama/student-interactive-profile/pages/blog.php">Blog</a></li>
     </ul>
 </nav>
